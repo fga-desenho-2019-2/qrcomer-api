c32dd124aa0c61a324cff034add625ea8985dc48
MatheusBlanco <msallesblanco@gmail.com>
2019-09-25 12:06:50 -0300

    Creating branch and modifying first models
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>

--- user_service/models.py
+++ user_service/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    full_name = models.CharField(max_lenght = 50)
+    email = models.EmailField(max_length = 20)
