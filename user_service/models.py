389f0be5368242d2095857f1e292fed913179370
MatheusBlanco <msallesblanco@gmail.com>
2019-09-27 12:39:31 -0300

    Adding test script and modifying models

--- user_service/models.py
+++ user_service/models.py
@@ -4,13 +4,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
 
-    first_name = models.CharField(blank = True, max_length = 50)
+    first_name = models.CharField(max_length = 50)
 
-    last_name = models.CharField(blank = True, max_length = 50)
+    last_name = models.CharField(max_length = 50)
 
     email = models.EmailField()
 
-    cpf = models.CharField(blank = True, max_length = 14)
+    cpf = models.CharField(primary_key = True, max_length = 14)
 
+    def __str__(self):
+        return self.first_name + self.last_name
+    
     EMAIL_FIELD = ['email']
     REQUIRED_FIELDS = ['email']
+    
--- user_service/tests.py
+++ user_service/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from model_mommy import mommy
+from .models import User
 
-# Create your tests here.
+class TestUser(TestCase):
+    def setUp(self):
+        self.user = mommy.make(User,
+            first_name = 'blanco',
+            last_name = 'matheus',
+            email = 'msallesblanco@gmail.com',
+            cpf = '065.822.021-70'
+        )
+        
+    def test_user_creation(self):
+        self.assertTrue(isinstance(self.user, User))
+        self.assertEquals(self.user.__str__(), self.user.first_name + self.user.last_name)
