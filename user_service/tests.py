45794f0b7a0070a75fd198f1edadce187d697711
Guilherme <marquesguilherme3@gmail.com>
2019-09-26 23:05:02 -0300

    Add test stage

--- user_service/tests.py
+++ user_service/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class UselessTest(TestCase):
+    def setUp(self):
+        print('Yay')
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        self.assertEqual(True, False)
