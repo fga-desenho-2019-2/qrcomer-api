2519faf730adde1bea8a57169d911ca42010b6a7
Pedro Rodrigues <peddroprp@gmail.com>
2019-10-01 08:41:47 -0300

    Correction in service name

--- dumpdata.sh
+++ dumpdata.sh
@@ -4,11 +4,11 @@ sleep 10
 docker-compose up -d api
 
 echo "Fazendo migrações"
-docker exec -it restaurant-service python manage.py makemigrations
-docker exec -it restaurant-service python manage.py migrate
+docker exec -it user-service python manage.py makemigrations
+docker exec -it user-service python manage.py migrate
 
 echo "Fazendo dump dos dados do banco de dados"
-docker exec -it restaurant-service python manage.py dumpdata > db/db.json
+docker exec -it user-service python manage.py dumpdata > db/db.json
 
 echo "Sucesso, backup salvo na pasta db"
 sudo docker-compose stop
