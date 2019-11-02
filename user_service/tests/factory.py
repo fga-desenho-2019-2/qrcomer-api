ef37843565d6f572b6de49e710c0cc011c55bc02
Elias Bernardo <ebmm01@gmail.com>
2019-11-02 14:40:04 -0300

    Refactoring tests to use UserFactory

--- user_service/tests/factory.py
+++ user_service/tests/factory.py
@@ -1,9 +1,17 @@
 
 import factory
 from ..api.utils.cpf import generate_cpf
+import factory.fuzzy
 
 class UserFactory(factory.django.DjangoModelFactory):
 
+    GENDERS = (
+        (0, 'm'),
+        (1, 'm'),
+    )
+    GENDER_IDS = [x[0] for x in GENDERS]
+
+    
     class Meta:
         model = 'user_service.Profile'
 
@@ -14,5 +22,5 @@ class UserFactory(factory.django.DjangoModelFactory):
     first_name = factory.Faker('first_name')
     birth_date = factory.Faker('date_of_birth', tzinfo=None, minimum_age=18, maximum_age=60)
     last_name = factory.Faker('last_name')
-    sex = factory.Iterator(['m', 'f'])
+    sex = factory.fuzzy.FuzzyChoice(GENDER_IDS)
 
--- user_service/tests/tests.py
+++ user_service/tests/tests.py
@@ -1,117 +1,56 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
-from .models import Profile
+from ..models import Profile
+from django.forms.models import model_to_dict
 from django.urls import reverse
+from .factory import UserFactory
+import factory
 
-
-class ProfileUserTest(APITestCase):
+class TestProfileUser(APITestCase):
     url = reverse('post_user')
 
+    def setUp(self):
+        self.user_data = factory.build(dict, FACTORY_CLASS=UserFactory)
+    
     def test_create_profile(self):
-
-        data = {
-                'cpf': '06066875132',
-                'first_name': 'Leonardo',
-                'last_name': 'barreiros',
-                'birth_date': '1996-11-14',
-                'sex': 'm',
-                'email': 'leossb@teste.com',
-                'password': 'teste@!'
-        }
-        response = self.client.post(self.url, data, format='json')
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Profile.objects.count(), 1)
 
     def test_create_profile_with_wrong_cpf_number_values(self):
-        data = {
-            'cpf': '68532168532',
-            'first_name': 'Leonardo',
-            'last_name': 'barreiros',
-            'birth_date': '1996-11-14',
-            'sex': 'm',
-            'email': 'leossb@teste.com',
-            'password': 'teste@!'
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.user_data['cpf'] = '68532168532'
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
     def test_create_profile_with_wrong_cpf_format(self):
-        data = {
-            'cpf': '653210',
-            'first_name': 'Leonardo',
-            'last_name': 'barreiros',
-            'birth_date': '1996-11-14',
-            'sex': 'm',
-            'email': 'leossb@teste.com',
-            'password': 'teste@!'
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.user_data['cpf'] = '653210'
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_profile_with_wrong_password(self):
-        data = {
-            'cpf': '06066875132',
-            'first_name': 'Leonardo',
-            'last_name': 'barreiros',
-            'birth_date': '1996-11-14',
-            'sex': 'm',
-            'email': 'leossb@teste.com',
-            'password': 'asd'
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.user_data['password'] = '123'
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_profile_without_first_name(self):
-        data = {
-            'cpf': '06066875132',
-            'first_name': '',
-            'last_name': 'barreiros',
-            'birth_date': '1996-11-14',
-            'sex': 'm',
-            'email': 'leossb@teste.com',
-            'password': 'teste@!'
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.user_data['first_name'] = ''
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Profile.objects.count(), 1)
 
     def test_create_profile_without_last_name(self):
-        data = {
-            'cpf': '06066875132',
-            'first_name': 'Leonardo',
-            'last_name': '',
-            'birth_date': '1996-11-14',
-            'sex': 'm',
-            'email': 'leossb@teste.com',
-            'password': 'teste@!'
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.user_data['last_name'] = ''
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Profile.objects.count(), 1)
 
     def test_create_profile_with_wrong_birth_date_format(self):
-        data = {
-            'cpf': '06066875132',
-            'first_name': 'Leonardo',
-            'last_name': '',
-            'birth_date': '15-11-1996',
-            'sex': 'm',
-            'email': 'leossb@teste.com',
-            'password': 'teste@!'
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.user_data['birth_date'] = '15-11-1996'
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_profile_with_birth_date_less_than_eighteen(self):
-        data = {
-            'cpf': '06066875132',
-            'first_name': 'Leonardo',
-            'last_name': '',
-            'birth_date': '2005-08-20',
-            'sex': 'm',
-            'email': 'leossb@teste.com',
-            'password': 'teste@!'
-        }
-        response = self.client.post(self.url, data, format='json')
+        self.user_data['birth_date'] = '2005-08-20'
+        response = self.client.post(self.url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
